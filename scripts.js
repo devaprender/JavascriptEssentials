72be92d20373c50d5c5ea0a9ce296c7c4288d54c
Jhonatan-de-Souza <jdsjhonatan@hotmail.com>
2018-11-12 06:59:46 -0200

    Javascript aula 05 - Objetos

--- scripts.js
+++ scripts.js
@@ -2,4 +2,12 @@ let nome = 'Rafael';
 let idade = 25; 
 let estaAprovado = true; 
 let sobrenome = undefined;
-let corSelecionado = null;
+
+let pessoa = {
+    nome: 'Rafael',
+    idade: 25,
+    estaAprovado: true,
+    sobrenome : 'de souza'
+ };
+
+ console.log(pessoa);
