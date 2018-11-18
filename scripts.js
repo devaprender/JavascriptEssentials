7f65337c88ce29c13bf35de70354376bf4a1b3d2
Jhonatan-de-Souza <jdsjhonatan@hotmail.com>
2018-11-18 11:09:30 -0200

    Javascript aula 11 - Operadores de Atribuição

--- scripts.js
+++ scripts.js
@@ -1,22 +1,5 @@
-// // Operadores Aritiméticos
-// let salario = 100;
-
-// //  + , - ,* , / , **
-
-// console.log(salario + salario);
-// console.log(salario - salario);
-// console.log(salario * salario);
-// console.log(salario / salario);
-// console.log(5 ** 5);
-
-// ++ --
-
-let idade = 18;
-
-console.log(--idade);
-console.log(idade);
-
-
-
-
-
+let valorTecladoGamer = 100;
+// valorTecladoGamer = valorTecladoGamer + valorTecladoGamer;
+// valorTecladoGamer = valorTecladoGamer - valorTecladoGamer;
+valorTecladoGamer -= valorTecladoGamer;
+console.log(valorTecladoGamer);
