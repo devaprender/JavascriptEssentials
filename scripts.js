5128d5186c92bda2ee37d248aa092461e349440a
Jhonatan-de-Souza <jdsjhonatan@hotmail.com>
2018-11-18 11:08:47 -0200

    Javascript Aual 10 - Operadores Aritiméticos

--- scripts.js
+++ scripts.js
@@ -1,5 +1,22 @@
-// Operadores Aritiméticos(matemáticos)
-// Operadores Atribuição
-// Operadores de Comparaçaõ
-// Operadores Lógicos
-// Operadores Bitwise
+// // Operadores Aritiméticos
+// let salario = 100;
+
+// //  + , - ,* , / , **
+
+// console.log(salario + salario);
+// console.log(salario - salario);
+// console.log(salario * salario);
+// console.log(salario / salario);
+// console.log(5 ** 5);
+
+// ++ --
+
+let idade = 18;
+
+console.log(--idade);
+console.log(idade);
+
+
+
+
+
