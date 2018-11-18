e16608302eb748ff2267ed1324b764182eb399c2
Jhonatan-de-Souza <jdsjhonatan@hotmail.com>
2018-11-18 11:15:08 -0200

    Javascript Aula 12 - Operadores Lógicos

--- scripts.js
+++ scripts.js
@@ -1,5 +1,9 @@
-let valorTecladoGamer = 100;
-// valorTecladoGamer = valorTecladoGamer + valorTecladoGamer;
-// valorTecladoGamer = valorTecladoGamer - valorTecladoGamer;
-valorTecladoGamer -= valorTecladoGamer;
-console.log(valorTecladoGamer);
+//Operadores De Comparação
+//  > , < , >= , <=, === , !==
+console.log(5 > 10);
+console.log(5 < 10);
+console.log(5 >= 5);
+console.log(5 <= 5);
+
+console.log( 10 === 11);
+console.log( 20 !== 20);
