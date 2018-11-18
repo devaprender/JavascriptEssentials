e04f2a2e913b1cf6ea49ebff7d73810bb84c8a0f
Jhonatan-de-Souza <jdsjhonatan@hotmail.com>
2018-11-18 11:06:32 -0200

    Javascript Aula 09 - Tipos de Operadores

--- scripts.js
+++ scripts.js
@@ -1,16 +1,5 @@
-// Realizar um tarefa , não devolve nada
-function dizerNome(){
-    console.log('jhonatan');
-}
-
-dizerNome();
-// Faz um cálculo ou operação e retorna algo
-function MultiplicarPorDois(valor){
-    return valor* 2;
-}
-
-//console.log(MultiplicarPorDois(5));
-
-let resultado = MultiplicarPorDois(5);
-
-console.log(resultado);
+// Operadores Aritiméticos(matemáticos)
+// Operadores Atribuição
+// Operadores de Comparaçaõ
+// Operadores Lógicos
+// Operadores Bitwise
