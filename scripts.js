6c4ca70ae406adf450973d9755130683b6603a1e
Jhonatan-de-Souza <jdsjhonatan@hotmail.com>
2018-11-18 11:05:24 -0200

    Javascript Aula 08 - Tipos de Funções

--- scripts.js
+++ scripts.js
@@ -1,9 +1,16 @@
-// Verbo + Substantivo
-let corSite = "azul";
-function resetaCor(cor,tonalidade){
-    corSite = cor + ' ' + tonalidade;
-};
+// Realizar um tarefa , não devolve nada
+function dizerNome(){
+    console.log('jhonatan');
+}
 
-console.log(corSite);
-resetaCor("verde","claro");
-console.log(corSite);
+dizerNome();
+// Faz um cálculo ou operação e retorna algo
+function MultiplicarPorDois(valor){
+    return valor* 2;
+}
+
+//console.log(MultiplicarPorDois(5));
+
+let resultado = MultiplicarPorDois(5);
+
+console.log(resultado);
