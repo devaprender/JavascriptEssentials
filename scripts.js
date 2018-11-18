69fcc9cd90f78fdd1b4c9696f4c38e0bcab7d868
Jhonatan-de-Souza <jdsjhonatan@hotmail.com>
2018-11-18 11:41:21 -0200

    Javascript aula 14 - Operadores de Igualdade

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
-// Operadores de igualdade
-// Igualdade estrita 
-// console.log ( 1 === 1);
- //console.log( '1' === 1 );
-
-// Igualdade solta
-console.log ( 1 == 1 );
-console.log ( '1' == 1);
+-// Operadores de igualdade
+-// Igualdade estrita
+-// console.log ( 1 === 1);
+- //console.log( '1' === 1 );
+-
+-// Igualdade solta
+-console.log ( 1 == 1 );
+-console.log ( '1' == 1);
