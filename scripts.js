636875864a0ac33b36a743a21e96585ca4922acd
Jhonatan-de-Souza <jdsjhonatan@hotmail.com>
2018-12-02 12:08:41 -0200

    Javascript Aula 14 - Operadores Lógicos

--- scripts.js
+++ scripts.js
@@ -1,8 +1,21 @@
--// Operadores de igualdade
--// Igualdade estrita
--// console.log ( 1 === 1);
-- //console.log( '1' === 1 );
--
--// Igualdade solta
--console.log ( 1 == 1 );
--console.log ( '1' == 1);
+// Operador Lógico e (&&) 
+// Retorna TRUE se os dois operandos forem true
+
+//console.log(true && false);
+//Operador Lógico ou (||)
+//Retornar true se um dos operandos forem true
+let maiorDeIdade = false;
+let possuiCarteiraDeTrabalho = false;
+let podeAplicar = maiorDeIdade || possuiCarteiraDeTrabalho;
+
+console.log('pode aplicar: ',podeAplicar);
+
+// Operador NOT (!)
+let candidatoRecusado = !podeAplicar;
+
+console.log('Candidato Recusado',candidatoRecusado);
+
+
+
+
+
