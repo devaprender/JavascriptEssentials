799dae16cef6b00ffe4cb0d9406fa745b59d9564
Jhonatan-de-Souza <jdsjhonatan@hotmail.com>
2019-01-01 23:25:50 -0200

    Javascript Condicionais - Aula 1

--- scripts.js
+++ scripts.js
@@ -1,9 +1,16 @@
-let a  = 'vermelho';
-let b = 'azul';
+// If..Else
 
-let c = a;
-a = b;
-b = c;
+// Se a hora estiver entre 06:00 até 12:00 : Bom dia!
+// Se estiver entre 12:00 até 18:00 : Boa tarde!
+// Caso contrário : Boa noite!
+let hora = 22;
 
-console.log(a);
-console.log(b);
+if (hora > 6 && hora < 12) {
+    console.log("bom dia");
+}
+else if (hora > 12 && hora < 18) {
+    console.log("Boa tarde");
+}
+else {
+ console.log("Boa noite");
+}
