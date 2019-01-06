674de763f04335862020b7b7199a9e89dd3f5e9f
Jhonatan-de-Souza <jdsjhonatan@hotmail.com>
2019-01-06 16:35:07 -0200

    Javascript Tutorial - Do..While

--- scripts.js
+++ scripts.js
@@ -1,23 +1,6 @@
-// console.log('Estou aprendendo!');
-// console.log('Estou aprendendo!');
-// console.log('Estou aprendendo!');
-// console.log('Estou aprendendo!');
-// console.log('Estou aprendendo!');
-
-// 1.For
-
-for(let i = 5; i >= 1; i--) {
-    if(i % 2 !== 0){ 
-        console.log(i);
-    }
-}
-
-// While loop
-let i = 5;
-
-while (i >= 1) {
-    if(i % 2 !== 0){ 
-        console.log(i);
-    }
-    i--;
-}
+// Do..While
+let i = 0;
+do {
+    console.log('digitando!',i);
+    i++;
+} while (i < 10)
