d0a29184c118739eb1b088e5798c61609462f495
Jhonatan-de-Souza <jdsjhonatan@hotmail.com>
2019-01-14 07:23:04 -0200

    Javascript Aula 21 - For in Loop

--- scripts.js
+++ scripts.js
@@ -1,6 +1,22 @@
-// Do..While
-let i = 0;
-do {
-    console.log('digitando!',i);
-    i++;
-} while (i < 10)
+// for
+// while
+// do-while
+
+//for-in
+
+const pessoa = {
+    nome: 'Jhonatan',
+    idade: 25
+};
+
+for(let chave in pessoa) {
+    console.log(chave,pessoa['nome']);
+}
+
+const cores = ['Vermelho','Azul','Verde'];
+
+for (let indice in cores) {
+    console.log(indice,cores[indice])
+}
+
+//for-of
