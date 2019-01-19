0788a41cc177de3eaf60a26d78f20c54fc5e53c0
Jhonatan-de-Souza <jdsjhonatan@hotmail.com>
2019-01-19 12:03:46 -0200

    Javascript Aula 24 - Exercício 2 Máximo entre dois valores

--- scripts.js
+++ scripts.js
@@ -1,22 +1,8 @@
-// for
-// while
-// do-while
+// Escreva uma função que usa 2 números e retorna o maior entre eles
 
-//for-in
+let valorMaior = max(10,10);
+console.log(valorMaior);
 
-const pessoa = {
-    nome: 'Jhonatan',
-    idade: 25
-};
-
-for(let chave in pessoa) {
-    console.log(chave,pessoa['nome']);
+function max(numero1,numero2) {
+     return numero1 > numero2 ? numero1: numero2 ;
 }
-
-const cores = ['Vermelho','Azul','Verde'];
-
-for (let indice in cores) {
-    console.log(indice,cores[indice])
-}
-
-//for-of
