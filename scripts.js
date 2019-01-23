50b756ffda0a2eac2f00c5d38b29014f93917d62
Jhonatan-de-Souza <jdsjhonatan@hotmail.com>
2019-01-23 07:29:19 -0200

    Javascrupt Aula 25 - FizzBuzz

--- scripts.js
+++ scripts.js
@@ -1,8 +1,20 @@
-// Escreva uma função que usa 2 números e retorna o maior entre eles
+// Divisível por 3 => Fizz
+// Divisível por 5 => Buzz
+// Divisível por 3 e 5 => FizzBuzz
+// Não divisível por 3 ou 5 => entrada
+// Não é um número => 'Não é um número'
+const resultado = fizzBuzz(11);
+console.log(resultado);
 
-let valorMaior = max(10,10);
-console.log(valorMaior);
-
-function max(numero1,numero2) {
-     return numero1 > numero2 ? numero1: numero2 ;
-}
+function fizzBuzz(entrada){
+    if ( typeof entrada !== 'number')
+        return 'Não é um número';
+    if ((entrada % 3 === 0) && (entrada % 5 === 0))
+        return 'FizzBuzz';
+    if (entrada % 3 === 0)
+        return 'Fizz';
+    if (entrada % 5 === 0)
+        return 'Buzz';
+    
+    return entrada;
+ }
