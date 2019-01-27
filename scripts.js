f0b7c5092152f34ab5f39d72018ca8654c292d2d
Jhonatan-de-Souza <jdsjhonatan@hotmail.com>
2019-01-27 14:20:01 -0200

    Javascript Aula 29 - Multiplos de 3 e 5

--- scripts.js
+++ scripts.js
@@ -1,15 +1,25 @@
-// Criar um método para ler propriedades de um objeto e 
-// exibir somente as propriedades do tipo string que estão nesse objeto
+// Criar função somar que retorna a 
+// soma de todos os múltiplos de 3 e 5
 
-const filme = {
-    titulo : 'Vingadores',
-    ano : 2018,
-    diretor: 'Robin',
-    personagem : 'Thor'
-}
-exibirPropriedades(filme);
-function exibirPropriedades(obj) {
-    for (prop in obj)
-        if(typeof obj[prop] === 'string')
-            console.log(prop,obj[prop])
+// Multiplos de 3
+// 3,6,9
+// Multiplos de 5
+// 5,10
+
+//Somando os multiplos 
+// armazenar os multiplos de 3
+// armazenar os multiplos de 5
+// somar os dois
+
+somar(10);
+function somar(limite) {
+   let multiplosDe3 = 0;
+   let multiploDe5 = 0;
+   for(i =0;i <= limite;i++){
+       if(i % 3 === 0)
+        multiplosDe3 += i;
+        if(i % 5 === 0)
+        multiploDe5 += i;
+   }
+   console.log(multiplosDe3 + multiploDe5);
 }
