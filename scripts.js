831eaef9ca856ca64518bb66f1ad143159ac7a62
Jhonatan-de-Souza <jdsjhonatan@hotmail.com>
2019-02-05 10:47:59 -0200

    Javascript Aula 31 - Contador de Asteríscos

--- scripts.js
+++ scripts.js
@@ -1,30 +1,19 @@
-// Exercício Nota Escolar
-// Obter a média a partir de um array
+// Criar uma função que exibe a quantidade de * 
+// que aquela linha possui
+exibirAsteriscos(10);
 
-// 0-59: F
-// 60-69: D
-// 70-79: C
-// 80-89: D
-// 90-100 : A
-const array = [70,70,80];
-// 73.33
-
-console.log(mediaDoAluno(array));
-
-function mediaDoAluno(notas) {
-      const media = calcularMedia(notas);
-
-      if (media < 59) return 'F';
-      if (media < 69) return 'D';
-      if (media < 79) return 'C';
-      if (media < 89) return 'B';
-      return 'A'
-}
-
-function calcularMedia(array) {
-      let soma = 0;
-      for (let valor of array) {
-            soma += valor;
+function exibirAsteriscos(linhas) {
+//     let padrao = '';
+//     for(let linha = 1; linha <= linhas; linha++){
+//           padrao += '*';
+//           console.log(padrao);
+//     }
+      for(let linha = 1; linha <= linhas; linha++){
+            let padrao = '';
+            for(let i = 0; i < linha; i++){
+                  padrao += '*';
+            }
+            console.log(padrao);
       }
-      return soma/(array.length);
 }
+
