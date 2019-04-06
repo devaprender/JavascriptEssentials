cb5c272368cc99ca20961718660a3671acfa5485
Jhonatan-de-Souza <jdsjhonatan@hotmail.com>
2019-04-06 07:52:53 -0300

    Javascript Aula 52 - Removendo Elementos de um Array

--- scripts.js
+++ scripts.js
@@ -1,17 +1,20 @@
-// JAVASCRIPT AULA 57 REMOVENDO ELEMENTOS 
-const numeros = [1,2,3,4,5,6];
-numeros.push();
-numeros.unshift();
-numeros.splice();
+// JAVASCRIPT AULA 58 ESVAZIANDO UM ARRAY
+let numeros = [1,2,3,4,5,6];
+let outros = numeros;
+// Solução 1
+// numeros = [];
+// console.log(outros);
 
-//Final
-const ultimo = numeros.pop();
-console.log(ultimo);
-console.log(numeros);
-// Início
-const primeiro = numeros.shift();
-console.log(primeiro);
-console.log(numeros);
-// Meio
-const meio = numeros.splice(2,1);
-console.log(meio);
+// Solução 2 
+numeros.length = 0;
+// console.log(numeros);
+// console.log(outros);
+
+// Solução 3
+// numeros.splice(0,numeros.length);
+// console.log(numeros);
+// console.log(outros);
+
+// Solução 4
+// while (numeros.length > 0)
+//     numeros.pop();
